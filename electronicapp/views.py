e4024551afbc801fa0c020d874b4b84afe30d32b
Andrey Ryazanov <ark2635@gmail.com>
2024-01-24 18:54:29 +0300

    upd views

--- electronicapp/views.py
+++ electronicapp/views.py
@@ -8,3 +8,13 @@ class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
     permission_classes = [IsActiveEmployee]
+
+    def get_queryset(self):
+        country_filter = self.request.query_params.get('country', None)
+        queryset = super().get_queryset()
+        if country_filter:
+            queryset = queryset.filter(country=country_filter)
+        return queryset
+
+    def perform_update(self, serializer):
+        serializer.save(debt_to_supplier=self.get_object().debt_to_supplier)
